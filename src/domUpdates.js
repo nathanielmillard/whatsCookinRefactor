1bbcf99c81f5537d1fb7fa453724019d9b2eb48b
Stacy Potten <stacylynnaz@gmail.com>
2020-09-13 12:55:44 -0600

    Add getWhatsCookingData method with promise all.

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,11 +1,26 @@
 let domUpdates = {
 
   users: [{}],
-  serverRecipeData: [{}],
+  recipeData: [{}],
   ingredientsData: [{}],
   user: //class instance,
   cookbook: //class instance,
   pantry: //class instance,
+
+  getWhatsCookingData() {
+    Promise.all([
+      fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/users/wcUsersData'),
+      fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/recipes/recipeData'),
+      fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/ingredients/ingredientsData')
+    ])
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(responses => {
+      this.users = responses[0];
+      this.recipeData = responses[1];
+      this.ingredientsData = responses[2];
+    })
+    .catch(error => console.log(error))
+  }
 
   
 
