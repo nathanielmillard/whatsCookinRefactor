fdb51c13eacc9a0381c4b3ff62f236f93ff1136f
Stacy Potten <stacylynnaz@gmail.com>
2020-09-13 14:16:53 -0600

    Remove unused lines of code. Import domUpdates methods.

--- src/scripts.js
+++ src/scripts.js
@@ -1,38 +1,7 @@
 // import './css/base.scss';
 import './css/styles.scss';
 
-// import recipeData from './data/recipes';
-// import ingredientsData from './data/ingredients';
-// import users from './data/users';
-
-let users = [{}];
-let serverRecipeData = [{}];
-let ingredientsData = [{}];
-
-fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/users/wcUsersData')
-  .then(response => response.json())
-  .then(data => {
-    users = data.wcUsersData
-    onStartup(users[1]);
-  })
-  .catch(err => console.log('error'))
-
-fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/recipes/recipeData')
-  .then(response => response.json())
-  .then(recipeData => {
-    serverRecipeData = recipeData.recipeData
-    onStartup(users[1]);
-  })
-  .catch(err => console.log('error'))
-
-fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/ingredients/ingredientsData')
-  .then(response => response.json())
-  .then(data => {
-    ingredientsData = data.ingredientsData
-    onStartup(users[1]);
-  })
-  .catch(err => console.log('error'))
-
+import domUpdates from './domUpdates';
 
 import Pantry from './pantry';
 import Recipe from './recipe';
@@ -42,63 +11,9 @@ import Cookbook from './cookbook';
 const showFavoritesButton = document.querySelector('.view-favorites');
 const homeButton = document.querySelector('.home')
 const cardArea = document.querySelector('.all-cards');
-const welcomeMessage = document.querySelector('.greeting');
 const searchBar = document.querySelector('.search-input');
 
 let domUser, pantry, cookbook;
-
-const onStartup = (user) => {
-  let recipeDeck = [];
-  serverRecipeData.forEach((recipe) => {
-    let instance = new Recipe(recipe, ingredientsData)
-    recipeDeck.push(instance)
-  });
-  cookbook = new Cookbook(recipeDeck);
-  pantry = new Pantry(users[1].pantry);
-  domUser = new User(user, pantry);
-  populateCards(cookbook.recipes);
-  greetUser();
-}
-
-const greetUser = () => {
-  welcomeMessage.innerText = `Welcome ${domUser.name}!`;
-};
-
-const populateCards = (recipes) => {
-  cardArea.innerHTML = '';
-  // if (cardArea.classList.contains('all')) {
-  //   cardArea.classList.remove('all') // what does this all do?
-  // }
-  recipes.forEach(recipe => {
-    cardArea.insertAdjacentHTML('afterbegin', constructCard(recipe))
-  });
-  getFavorites();
-};
-
-const getFavorites = () => {
-  if (domUser.favoriteRecipes.length) {
-    domUser.favoriteRecipes.forEach(recipe => {
-      document.querySelector(`.favorite${recipe.id}`).classList.add('favorite-active')
-    })
-  }
-}
-
-const constructCard = (recipe) => {
-  return `<div class='card ${recipe.id}'>
-  <header class='card-header ${recipe.id}'>
-    <label for='add-button' class='hidden'>Click to add recipe</label>
-    <button aria-label='add-button' class='add-button card-button ${recipe.id}'>
-      <img class='add ${recipe.id} add-button' src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
-      recipes to cook'>
-    </button>
-    <label for='favorite-button' class='hidden'>Click to favorite recipe</label>
-    <button aria-label='favorite-button' class='favorite card-button favorite${recipe.id} ${recipe.id}'>
-    </button>
-  </header>
-  <span class='recipe-name ${recipe.id}'>${recipe.name}</span>
-    <img tabindex='0' class='card-picture ${recipe.id}'src='${recipe.image}' alt='Food from recipe'>
-  </div>`
-};
 
 const favoriteCard = (event) => {
   let specificRecipe = cookbook.recipes.find(recipe => {
@@ -148,7 +63,6 @@ const viewFavorites = () => {
   }
 };
 
-
 const displayDirections = (event) => {
   let newRecipe = cookbook.recipes.find(recipe => {
     if (event.target.classList.contains(`${recipe.id}`)) {
@@ -180,7 +94,6 @@ const displayDirections = (event) => {
   </div>`;
 };
 
-// for display 'to cook' cards function
 const displayToCookCards = () => {
   cardArea.innerHTML =
     `<section class='to-cook'>
@@ -220,7 +133,7 @@ let cardSection = document.querySelector('.card-section')
 </div>`);
   });
 };
-// for display 'to cook' cards function
+
 const showToCookButton = document.querySelector('.to-cook');
 const showToCookRecipes = () => {
 if (!domUser.recipesToCook.length) {
@@ -276,6 +189,7 @@ const cardButtonConditionals = (event) => {
   }
 }
 
+window.onload = domUpdates.getWhatsCookingData()
 homeButton.addEventListener('click', cardButtonConditionals);
 cardArea.addEventListener('click', cardButtonConditionals);
 showFavoritesButton.addEventListener('click', viewFavorites);
