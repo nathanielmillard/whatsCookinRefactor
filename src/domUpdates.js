879c50ceb330e04f7217429cf4b58bc6a875938b
Stacy Potten <stacylynnaz@gmail.com>
2020-09-13 12:37:01 -0600

    Create domUpdates file and initial keys

---
+++ src/domUpdates.js
@@ -0,0 +1,14 @@
+let domUpdates = {
+
+  users: [{}],
+  serverRecipeData: [{}],
+  ingredientsData: [{}],
+  user: //class instance,
+  cookbook: //class instance,
+  pantry: //class instance,
+
+  
+
+};
+
+export default  domUpdates;
