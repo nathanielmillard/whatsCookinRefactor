80f8b1245ce27e99b6bf2ffd266843f1ec8b0574
Tim Keresey <tim.keresey@gmail.com>
2020-09-14 09:38:47 -0600

    revise syntax in spies

--- test/spies.js
+++ test/spies.js
@@ -1,15 +1,17 @@
 import { expect } from 'chai';
-import spies from 'chai-spies';
+const chai = require('chai');
+const spies = require('chai-spies');
 chai.use(spies);
 
 //import domUpdates?
 
-describe('domUpdates', () => {
+describe.only('domUpdates', () => {
 
   beforeEach(() => {
     global.domUpdates = {};
     chai.spy.on(domUpdates, [
       'getWhatsCookingData',
+      'updateDisplay',
       'greetUser',
       'populateCards',
       'favoriteCard',
@@ -59,21 +61,21 @@ describe('domUpdates', () => {
 
   it('should display to cook section', () => {
     domUpdates.showToCookRecipes();
-    expect(domUpdates.showToCookCards()).to.have.been.called(1);
+    expect(domUpdates.showToCookRecipes()).to.have.been.called(1);
   });
 
   it('should remove cards from to cook section', () => {
     domUpdates.removeFromToCook();
-    expect(removeFromToCook()).to.have.been.called(1);
+    expect(domUpdates.removeFromToCook()).to.have.been.called(1);
   });
 
   it('should show recipe directions', () => {
     domUpdates.displayDirections();
-    expect(displayDirections()).to.have.been.called.with('event');
+    expect(domUpdates.displayDirections()).to.have.been.called.with('event');
   });
 
   it('should filter recipes by search', () => {
     domUpdates.filterRecipes();
-    expect(filterRecipes()).to.have.been.called(1);
+    expect(domUpdates.filterRecipes()).to.have.been.called(1);
   });
 })
