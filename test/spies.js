dcc5d38eba7907a026f31e6cd4a83f2e7b6b7b01
Tim Keresey <tim.keresey@gmail.com>
2020-09-14 04:33:04 -0600

    create spies for instantiating data and displaying data

--- test/spies.js
+++ test/spies.js
@@ -1,4 +1,5 @@
-const spies = require('chai-spies');
+import { expect } from 'chai';
+import spies from 'chai-spies';
 chai.use(spies);
 
 describe('domUpdates', () => {
@@ -6,8 +7,11 @@ describe('domUpdates', () => {
   beforeEach(() => {
     global.domUpdates = {};
     chai.spy.on(domUpdates, [
+      'getWhatsCookingData',
       'createDataModel',
       'updateDisplay',
+      'greetUser',
+      'populateCards'
       'getFavorites',
       'favoriteCard',
       'viewFavorites',
@@ -24,5 +28,15 @@ describe('domUpdates', () => {
     chai.spy.restore(domUpdates);
   });
 
-  
+  it('should instantiate data', () => {
+    domUser.getWhatsCookingData();
+    expect(domUpdates.createDataModel()).to.have.been.called(1);
+  });
+
+  it('should display the instantiated data', () => {
+    domUser.getWhatsCookingData();
+    expect(domUpdates.updateDisplay()).to.have.been.called(1);
+  });
+
+
 })
