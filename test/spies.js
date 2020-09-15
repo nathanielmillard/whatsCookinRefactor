87fb559ea5e108b4378462f17a3ffc4efb78d7f9
Tim Keresey <tim.keresey@gmail.com>
2020-09-14 19:02:17 -0600

    revise syntax to resolve errors

--- test/spies.js
+++ test/spies.js
@@ -23,7 +23,7 @@ describe.only('domUpdates', () => {
       'removeFromToCook',
       'displayDirections',
       'filterRecipes'
-    ], () => true)
+    ], () => {})
   });
 
   afterEach(() => {
@@ -32,52 +32,52 @@ describe.only('domUpdates', () => {
 
   it('should instantiate data', () => {
     domUpdates.getWhatsCookingData();
-    expect(domUpdates.getWhatsCookingData()).to.have.been.called(1);
+    expect(domUpdates.getWhatsCookingData).to.have.been.called(1);
   });
 
   it('should greet the user', () => {
     domUpdates.greetUser();
-    expect(domUpdates.greetUser()).to.have.been.called(1);
+    expect(domUpdates.greetUser).to.have.been.called(1);
   });
 
   it('should display recipe cards', () => {
     domUpdates.updateDisplay();
-    expect(domUpdates.populateCards()).to.have.been.called.with('domUpdates.cookbook.recipes');
+    expect(domUpdates.populateCards).to.have.been.called.with('domUpdates.cookbook.recipes');
     //arguments?
   });
 
   it('should add cards to favorites', () => {
     domUpdates.favoriteCard();
-    expect(domUpdates.favoriteCard()).to.have.been.called(1);
+    expect(domUpdates.favoriteCard).to.have.been.called(1);
   });
 
   it('should show favorites', () => {
     domUpdates.viewFavorites();
-    expect(domUpdates.viewFavorites()).to.have.been.called(1);
+    expect(domUpdates.viewFavorites).to.have.been.called(1);
   });
 
   it('should add cards to to cook section', () => {
     domUpdates.addToToCook();
-    expect(domUpdates.addToToCook()).to.have.been.called.with('event');
+    expect(domUpdates.addToToCook).to.have.been.called.with('event');
   });
 
   it('should display to cook section', () => {
     domUpdates.showToCookRecipes();
-    expect(domUpdates.showToCookRecipes()).to.have.been.called(1);
+    expect(domUpdates.showToCookRecipes).to.have.been.called(1);
   });
 
   it('should remove cards from to cook section', () => {
     domUpdates.removeFromToCook();
-    expect(domUpdates.removeFromToCook()).to.have.been.called(1);
+    expect(domUpdates.removeFromToCook).to.have.been.called(1);
   });
 
   it('should show recipe directions', () => {
     domUpdates.displayDirections();
-    expect(domUpdates.displayDirections()).to.have.been.called.with('event');
+    expect(domUpdates.displayDirections).to.have.been.called.with('event');
   });
 
   it('should filter recipes by search', () => {
     domUpdates.filterRecipes();
-    expect(domUpdates.filterRecipes()).to.have.been.called(1);
+    expect(domUpdates.filterRecipes).to.have.been.called(1);
   });
 })
