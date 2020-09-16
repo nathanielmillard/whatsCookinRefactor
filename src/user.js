a45b43f37936475e8d2ee4daef97cbc679e641d4
Tim Keresey <tim.keresey@gmail.com>
2020-09-16 10:13:42 -0600

    fix lint errors on User

--- src/user.js
+++ src/user.js
@@ -13,13 +13,13 @@ class User {
   addToFavorites(recipe) {
     if (!this.favoriteRecipes.includes(recipe)) {
       this.favoriteRecipes.push(recipe);
-    };
+    }
   }
 
   addToRecipesToCook(recipe) {
     if (!this.recipesToCook.includes(recipe)) {
       this.recipesToCook.push(recipe);
-    };
+    }
   }
 
   removeFromRecipesToCook(recipe) {
@@ -30,16 +30,16 @@ class User {
   removePantryIngridients(recipe) {
     recipe.ingredients.forEach((item) => {
       fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/users/wcUsersData', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            "userID": this.id,
-            "ingredientID": item.id,
-            "ingredientModification": -(item.quantity.amount)
-          })
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "userID": this.id,
+          "ingredientID": item.id,
+          "ingredientModification": -(item.quantity.amount)
         })
+      })
         .then(response => response.json())
         .then(this.updateUserPantry())
         .catch(error => console.log(error));
@@ -51,16 +51,16 @@ class User {
   addNeededPantryIngridients(recipe) {
     this.checkHowMuchMore(recipe).forEach((item) => {
       fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/users/wcUsersData', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            "userID": this.id,
-            "ingredientID": item.id,
-            "ingredientModification": item.quantityNeeded
-          })
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "userID": this.id,
+          "ingredientID": item.id,
+          "ingredientModification": item.quantityNeeded
         })
+      })
         .then(response => response.json())
         .then(this.updateUserPantry())
         .catch(error => console.log(error));
@@ -75,7 +75,7 @@ class User {
         this.pantry = pantry
         this.pantry.compilePantry(this.ingredientsData)
       })
-      .catch(err => console.log(err))
+      .catch(error => console.log(error))
   }
 
   removeFromFavorites(recipe) {
@@ -102,7 +102,7 @@ class User {
     return recipe.ingredients.reduce((notPresent, ingredient) => {
       if (!this.pantry.userIngredients[ingredient.id] || ingredient.quantity.amount > this.pantry.userIngredients[ingredient.id]) {
         notPresent.push(ingredient);
-      };
+      }
       return notPresent;
     }, []);
   }
@@ -115,23 +115,23 @@ class User {
     } else {
       let missing = missingIngredients.length;
       return `You still need ${missing} more ingredients.`;
-    };
+    }
   }
 
   checkHowMuchMore(recipe) {
     let missingIngredients = this.findMissingIngredients(recipe);
     let difference = missingIngredients.map(ingredient => {
-        let name = recipe.ingredientsData.find(item => item.id === ingredient.id).name;
-        let neededIng = {
-          id: ingredient.id,
-          cost: 0,
-          name: name,
-          unit: ingredient.quantity.unit,
-          quantityNeeded: parseFloat(ingredient.quantity.amount.toFixed(2))
-        }
-        if (this.pantry.userIngredients[ingredient.id]) {
-          neededIng.quantityNeeded = parseFloat(ingredient.quantity.amount - this.pantry.userIngredients[ingredient.id].toFixed(2))
-        }
+      let name = recipe.ingredientsData.find(item => item.id === ingredient.id);
+      let neededIng = {
+        id: ingredient.id,
+        cost: 0,
+        name: name.name,
+        unit: ingredient.quantity.unit,
+        quantityNeeded: parseFloat(ingredient.quantity.amount.toFixed(2))
+      }
+      if (this.pantry.userIngredients[ingredient.id]) {
+        neededIng.quantityNeeded = parseFloat(ingredient.quantity.amount - this.pantry.userIngredients[ingredient.id].toFixed(2))
+      }
       let ingredientItem = recipe.ingredientsData.find(item => ingredient.id === item.id)
       neededIng.cost = (neededIng.quantityNeeded * ingredientItem.estimatedCostInCents) / 100;
       return neededIng;
